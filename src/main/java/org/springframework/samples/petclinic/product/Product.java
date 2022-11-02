0fb1002c18a1d5158de416ed10e197be85eae05b
diegarlin <garcialinaresdiego@gmail.com>
2022-11-02 22:47:46 +0100

    hasta el 8

--- src/main/java/org/springframework/samples/petclinic/product/Product.java
+++ src/main/java/org/springframework/samples/petclinic/product/Product.java
@@ -1,13 +1,45 @@
 package org.springframework.samples.petclinic.product;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.PositiveOrZero;
+import javax.validation.constraints.Size;
+
+import org.springframework.stereotype.Indexed;
+
 import lombok.Getter;
 import lombok.Setter;
 
+@Entity
 @Getter
 @Setter
+@Table(name = "products")
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Integer id;
+
+    @NotBlank
+    @Size(min = 3, max = 50)
     String name;
+
+    @NotNull
+    @PositiveOrZero
     double price;
+
+    @ManyToOne
+    @JoinColumn(name = "pTypeId")
     ProductType productType;
 }
